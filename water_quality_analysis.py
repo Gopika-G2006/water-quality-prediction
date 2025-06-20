aa2515d7b92b806861202e745510deeb60071e5e
Gopika G <gopikagopu.uk20@gmail.com>
2025-06-20 16:34:15 +0530

    Added water_quality_analysis.py for heatmap visualization

---
+++ water_quality_analysis.py
@@ -0,0 +1,14 @@
+from sklearn. model = LinearRegression() #building a linear model
+from sklearn. metrics import confusion_matrix, classification_report
+from scipy import stats
+import pandas as pd
+import matplotlib. pyplot as plt
+import seaborn as sns
+# Manually uploaded your CSV file to this when this runs.
+For Colab users, you can use the following cell to upload files:
+from google. colab import files
+uploaded = files.upload()
+df = pd. read_csv("water_quality. csv") # Make sure this file is uploaded in your Colab or local directory.
+print('Shape of the Dataset:', df. shape)
+print("Null data:\n", df. isnull(). sum())
+Drop missing values
